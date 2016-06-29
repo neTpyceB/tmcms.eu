8597a97586192928b20aa9ed436d3b6e4867f1bc
neTpyceB <pvman@inbox.ru>
2016-06-30 00:19:37 +0300

    configs

---
+++ configs/prod.php
@@ -0,0 +1,31 @@
+<?php
+
+// These are production server settings, shared between all developers
+$config_prod = [
+    'db' => [
+        'login' => 'db_user',
+        'password' => 'db_pass',
+        'name' => 'db_name'
+    ],
+    'site' => [
+        'email' => 'info@tmcms.eu',
+        'name' => 'The Modern CMS main site'
+    ],
+    'cms' => [
+        'unique_key' => 'registered_key', // Required for updates
+        'logo' => '/vendor/neTpyceB/TMCms/src/assets/images/logo.png', // Logo in admin panel
+        'logo_link' => 'http://devp.eu/', // Link on logo in admin panel
+    ],
+    'http_auth' => [
+        'login' => '', // Set if required
+        'password' => '',
+    ],
+];
+
+// Change all your local settings in local.php - file should return array with fields to be overwritten
+$config_local = [];
+if (file_exists(__DIR__ . '/local.php')) {
+    $config_local = include_once __DIR__ . '/local.php';
+}
+
+return array_merge($config_prod, $config_local);
